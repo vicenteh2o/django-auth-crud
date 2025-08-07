c63975278ff2dcffd05f2de0c58dd699faabf885
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-08-07 14:24:03 -0300

    feat: simple app tasks

--- authcrud/settings.py
+++ authcrud/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tasks',  # Custom app for task management
 ]
 
 MIDDLEWARE = [
