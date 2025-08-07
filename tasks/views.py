d2d555199ea1b40b057e86156516c99e05bebe78
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-08-07 14:50:20 -0300

    feat: simple wip signup form and home

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 
-def helloworld(request):
-    return HttpResponse('Hello, World')
+def home(request):
+    return render(request, 'home.html')
+
+
+def signup(request):
+    return render(request, 'signup.html', {'form': UserCreationForm()})
