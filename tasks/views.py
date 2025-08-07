a5443b07e539061402057f121b43f2309a86bd95
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-08-07 15:47:07 -0300

    feat: simple user creation

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 # Create your views here.
 
 
@@ -8,4 +10,16 @@ def home(request):
 
 
 def signup(request):
-    return render(request, 'signup.html', {'form': UserCreationForm()})
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                return HttpResponse("User created successfully")
+            except:
+                return HttpResponse("Username already exists")
+        return HttpResponse("Passwords dont match")
+    else:
+        print("Received GET request for signup")
+        return render(request, 'signup.html', {'form': UserCreationForm()})
