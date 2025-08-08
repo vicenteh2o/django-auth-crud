0256643e0eca6d02d2b8e3d1b67fbd0a4a5103a6
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-08-08 11:48:13 -0300

    feat: task detail

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
@@ -45,6 +45,11 @@ def create_task(request):
             return redirect('tasks')
         except ValueError:
             return render(request, 'create_task.html', {'form': TaskForm(), 'error': 'Bad data passed in. Try again.'})
+
+
+def task_detail(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    return render(request, 'task_detail.html', {'task': task})
 
 
 def signout(request):
