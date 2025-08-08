cd988fed03faa6c26f0afc6b2b9a4aa839894c1c
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-08-08 08:54:29 -0300

    feat: login handling

--- authcrud/urls.py
+++ authcrud/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('tasks/', views.tasks, name='tasks'),
     path('logout/', views.signout, name='logout'),
+    path('signin/', views.signin, name='signin'),
 ]
