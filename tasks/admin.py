3e5c25d5e8fb3e08c065de24c372423df3f937b9
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-08-08 09:43:17 -0300

    feat: task added on admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Task
+
+
+class TaskAdmin(admin.ModelAdmin):
+    readonly_fields = ('created',)
+    list_display = ('title', 'user', 'created', 'datecompleted', 'important')
+    search_fields = ('title', 'user__username')
+    list_filter = ('important', 'created')
+
 
 # Register your models here.
+admin.site.register(Task, TaskAdmin)
