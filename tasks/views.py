8288a41676d157d640955bb12b9670f33d5a4883
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-08-08 15:32:34 -0300

    feat: protect routes

--- tasks/views.py
+++ tasks/views.py
@@ -6,6 +6,7 @@ from django.db import IntegrityError
 from .forms import TaskForm
 from .models import Task
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -29,17 +30,20 @@ def signup(request):
         return render(request, 'signup.html', {'form': UserCreationForm()})
 
 
+@login_required
 def tasks(request):
     tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
     return render(request, 'tasks.html', {'tasks': tasks})
 
 
+@login_required
 def tasks_completed(request):
     tasks = Task.objects.filter(
         user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
     return render(request, 'tasks.html', {'tasks': tasks})
 
 
+@login_required
 def create_task(request):
     if request.method == 'GET':
         return render(request, 'create_task.html', {'form': TaskForm()})
@@ -54,6 +58,7 @@ def create_task(request):
             return render(request, 'create_task.html', {'form': TaskForm(), 'error': 'Bad data passed in. Try again.'})
 
 
+@login_required
 def task_detail(request, task_id):
     if request.method == 'GET':
         task = get_object_or_404(Task, pk=task_id, user=request.user)
@@ -69,6 +74,7 @@ def task_detail(request, task_id):
             return render(request, 'task_detail.html', {'task': task, 'form': form, 'error': 'Bad info'})
 
 
+@login_required
 def complete_task(request, task_id):
     task = get_object_or_404(Task, pk=task_id, user=request.user)
     if request.method == 'POST':
@@ -77,6 +83,7 @@ def complete_task(request, task_id):
         return redirect('tasks')
 
 
+@login_required
 def delete_task(request, task_id):
     task = get_object_or_404(Task, pk=task_id, user=request.user)
     if request.method == 'POST':
@@ -84,6 +91,7 @@ def delete_task(request, task_id):
         return redirect('tasks')
 
 
+@login_required
 def signout(request):
     logout(request)
     return redirect('home')
